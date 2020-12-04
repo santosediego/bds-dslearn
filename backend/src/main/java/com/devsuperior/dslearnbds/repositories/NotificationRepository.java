a531dcabf808250e64aa9c762773124a1c46d282
santosediego <diegoeustaquiodossantos@hotmail.com>
2020-12-04 07:11:22 -0300

    Custom content for current user, JPQL Query

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
@@ -1,8 +1,19 @@
 package com.devsuperior.dslearnbds.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslearnbds.entities.Notification;
+import com.devsuperior.dslearnbds.entities.User;
 
-public interface NotificationRepository  extends JpaRepository<Notification, Long>{
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+	// Se unreadOnly for false ele trará todos, se for true ele trará as não lidas;
+	@Query("SELECT obj FROM Notification obj WHERE"
+			+ "(obj.user = :user) AND"
+			+ "(:unreadOnly = false OR obj.read = false)"
+			+ "ORDER BY obj.moment DESC")
+	Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 }
